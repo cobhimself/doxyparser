98a798f373311af0eda22cae32471e8773c6acbb
Collin Brooks <cbrooks@sentryds.com>
2020-11-12 14:53:05 -0600

    Add comments and minor modifications

--- test/index/types/test_doxygen.py
+++ test/index/types/test_doxygen.py
@@ -1,11 +1,11 @@
-from doxyparser.index.types.compound import Compound
 from ... import helper
 import pytest
 
+# Expectations for our parsing of the index.xml doxygen file. We want the data
+# from inside 'compound'
 _dp_compound_expectations = helper.get_data_provider('test_index.yml', 'compound')
 
-@pytest.mark.parametrize("method,expectations", _dp_compound_expectations)
-def test_index_parser(method: str, expectations: list):
-    parser = helper.get_parser()
-    doxygen = parser.parse_index()
-    helper.confirm_node_expectations(doxygen, method, expectations)
+@pytest.mark.parametrize("node_getter,expectations", _dp_compound_expectations)
+def test_index_parser(node_getter: str, expectations: list):
+    doxygen = helper.get_parser().parse_index()
+    helper.confirm_node_expectations(doxygen, node_getter, expectations)
