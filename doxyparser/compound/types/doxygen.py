c962e2e01137a8cad2d73630eaeb09d8375f3c08
Collin Brooks <cbrooks@sentryds.com>
2020-11-13 13:19:29 -0600

    Add doxygen element in compound types

---
+++ doxyparser/compound/types/doxygen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Model representation of a doxygen element from doxygen's compound.xsd
+
+<xsd:element name="doxygen" type="DoxygenType"/>
+
+<xsd:complexType name="DoxygenType">
+  <xsd:sequence maxOccurs="unbounded">
+    <xsd:element name="compounddef" type="compounddefType" minOccurs="0" />
+  </xsd:sequence>
+  <xsd:attribute name="version" type="DoxVersionNumber" use="required" />
+  <xsd:attribute ref="xml:lang" use="required"/>
+</xsd:complexType>
+"""
+from ...node import Node
+
+class Doxygen(Node):
+    def __init__(self, node):
+        super.__init__(node)
+    
+    def get_compounddefs(self):
+      return self.get_children(
+        xsd = 'compound',
+        tag = 'compounddef'
+      )
+    
+    def get_version(self):
+      return self.get('version')
