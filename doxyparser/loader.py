d623316a10e6147304249a308e6deb2966670ece
Collin Brooks <cbrooks@sentryds.com>
2020-11-13 15:26:19 -0600

    Add exceptions when loading invalid xsd or tag

--- doxyparser/loader.py
+++ doxyparser/loader.py
@@ -12,6 +12,16 @@ class Loader():
     
     def load_tag_class(self, xsd, tag):
         if (xsd, tag) not in self._cache:
+
+            if xsd not in TAG_MAP.keys():
+                raise Exception(
+                    'Unknown doxyparser xsd type: ' + xsd + ' check TAG_MAP in doxyparser/__init__.py'
+                )
+            
+            if tag not in TAG_MAP[xsd].keys():
+                raise Exception(
+                    'No class mapping for tag ' + tag + 'is set! Update doxyparser/__init__.py'
+                )
             path = TAG_MAP[xsd][tag]
             parts = path.split('.')
             final = parts.pop()
@@ -25,4 +35,4 @@ class Loader():
 
     def load_refid(self, refid):
         tree = ET.parse(self._xml_dir + '/' + refid + '.xml')
-        return tree.getroot()
+        return tree
