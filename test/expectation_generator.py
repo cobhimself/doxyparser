d5c40f160427d4961f6ce79491887855b8fb78db
Collin Brooks <cbrooks@sentryds.com>
2020-11-19 14:05:51 -0600

    WIP

--- test/expectation_generator.py
+++ test/expectation_generator.py
@@ -1,4 +1,3 @@
-import json
 import sys
 import pathlib
 from xml.etree.ElementTree import ElementTree
@@ -8,20 +7,83 @@ try:
 except ImportError:
     from yaml import Dumper
 
+from loader import Loader
+from tree import Tree
+
+class ModelData():
+    def __init__(self, xsd):
+        self._xsd = xsd
+        self._loader = Loader()
+
+    def get_model_collections(self, tag):
+        pass
+
 
 class Expectations():
-    def __init__(self, tree):
-        self._tree = tree
 
-    def generate(self):
+    attr_value_map = {
+        'no': False,
+        'yes': True
+    }
+
+    attr_key_map = {
+        'bodyfile': 'body_file',
+        'bodystart': 'body_start',
+        'bodyend': 'body_end',
+        'refid': 'ref_id',
+        'kindref': 'kind_ref',
+    }
+
+    element_key_map = {
+        'sectiondef': 'section_defs',
+        'memberdef': 'member_def',
+        'argsstring': 'args_string',
+        'inbodydescription': 'in_body_description',
+        'detaileddescription': 'detailed_description',
+        'briefdescription': 'brief_description',
+        'basecompoundref': 'base_compound_ref',
+        'compoundname': 'compound_name',
+        'compounddef': 'compound_defs',
+        'childnode': 'child_node',
+        'collaborationgraph': 'collaboration_graph',
+    }
+
+    def __init__(self, tree, loader):
+        self._tree = tree
+        self._xsd = None
+        self._model_data = None
+
+    def generate(self, xsd, loader):
+        self._xsd = xsd
+        self._model_data = ModelData(xsd, loader)
         final = self.handle(self._tree.getroot())
         return final
+
+    def handle_attr(self, attributes):
+        final = {}
+        for key, value in attributes.items():
+            if key in self.attr_key_map.keys():
+                key = self.attr_key_map[key]
+
+            if value in self.attr_value_map.keys():
+                value = self.attr_value_map[value]
+
+            final[key] = value
+
+        return final
+
+    def handle_tag(self, tag):
+        if tag in self.element_key_map.keys():
+            return self.element_key_map[tag]
+
+        return tag
 
     def handle(self, element):
         is_complex = False
         final = {}
-        final[element.tag] = element.attrib
-        current = final[element.tag]
+        tag = self.handle_tag(element.tag)
+        final[tag] = self.handle_attr(element.attrib)
+        current = final[tag]
 
         text = '' if element.text is None else element.text.strip()
         tail = '' if element.tail is None else element.tail.strip()
@@ -39,21 +101,22 @@ class Expectations():
 
             for child in list(element):
                 data = self.handle(child)
+                child_tag = self.handle_tag(child.tag)
                 if is_complex:
                     # Did we have tail text?
                     tail = data.pop('_tail', None)
-                    current['_complex'].append({child.tag: data})
+                    current['_complex'].append({child_tag: data})
                     if tail is not None:
                         current['_complex'].append(tail)
                 else:
-                    if (isinstance(current, dict) and child.tag in current.keys()):
+                    if (isinstance(current, dict) and child_tag in current.keys()):
                         # We've seen this child tag before...
-                        if not isinstance(current[child.tag], list):
-                            previous_data = current[child.tag]
-                            current[child.tag] = [previous_data]
-                        current[child.tag].append(data)
+                        if not isinstance(current[child_tag], list):
+                            previous_data = current[child_tag]
+                            current[child_tag] = [previous_data]
+                        current[child_tag].append(data)
                     else:
-                        current[child.tag] = data
+                        current[child_tag] = data
 
         return current
 
@@ -63,12 +126,14 @@ class ExpectationGenerator():
         here = str(pathlib.Path(__file__).parent)
         xmldir = here + '/_sample_data/_build/php/xml/'
         ymldir = here + '/_expectation_data/'
-        self._xmlfile = xmldir + xmlfile + '.xml'
+        self._xmlfile = xmlfile
         self._ymloutput = ymldir + output + '.yml'
+        self._loader = Loader(xmldir)
+        self._xsd = 'index' if self._xmlfile.endswith('index.xml') else 'compound'
 
     def generate(self):
-        tree = ElementTree(file=(self._xmlfile))
-        self.write_yml(Expectations(tree).generate())
+        tree = self._loader.load(self._xmlfile)
+        self.write_yml(Expectations(tree).generate(self._xsd, self._loader))
 
     def write_yml(self, final):
         stream = open(self._ymloutput, 'w')
