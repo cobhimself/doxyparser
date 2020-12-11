3286e696c337067d05fdc30f0e74a2a56850c72e
Collin Brooks <cbrooks@sentryds.com>
2020-12-11 13:07:17 -0600

    Have cache use the new types module

--- doxyparser/util/generator/cache.py
+++ doxyparser/util/generator/cache.py
@@ -1,3 +1,5 @@
+from .types import Group, Type, Element
+
 group_cache = {}
 type_cache = {}
 element_cache = []
@@ -18,15 +20,12 @@ def get_type(type_name):
     return type_cache.get(type_name)
 
 def add_group(group):
-    from doxyparser.util.generator.group import Group
     group_cache[group.name] = Group(group)
 
 def add_type(node_type):
-    from doxyparser.util.generator.type import Type
     type_cache[node_type.name] = Type(node_type)
 
 def add_element(element):
-    from doxyparser.util.generator.element import Element
     element_cache.append(Element(
         element,
         get_type(element.type.name)
