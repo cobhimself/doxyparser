99278cfa663bf357b266ae448d07c825ffdf90fc
Collin Brooks <cbrooks@sentryds.com>
2020-12-11 10:09:32 -0600

    Update buildable.py documentation

--- doxyparser/util/generator/buildable.py
+++ doxyparser/util/generator/buildable.py
@@ -1,9 +1,36 @@
+"""
+MIT License
+
+Copyright (c) 2020 Collin Brooks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 from xmlschema.validators import XsdGroup
 from xmlschema.validators.elements import XsdElement
 from xmlschema.validators.attributes import XsdAttributeGroup, XsdAttribute, XsdAnyAttribute
 from .super import Super
 
+
 class Buildable(Super):
+    """Provides a class with the ability to build doxyparser-specific data
+    relating to XSD attributes, elements and groups.
+    """
     def __init__(self, definition):
         super().__init__(definition)
         self._attr = {}
@@ -39,7 +66,6 @@ class Buildable(Super):
                 for attr_name, attr in definition.attributes.items():
                     self._attr[attr_name] = Attribute(attr, self)
 
-
         self._built = True
 
     def _build_group(self, group):
@@ -59,26 +85,27 @@ class Buildable(Super):
         else:
             for component in group.iter_components():
                 is_group = isinstance(component, XsdGroup)
-                #Iterating the components usually returns the group itself so
-                #we check to make sure our component isn't the group itself.
-                #Also, for groups within groups, we don't want to traverse
-                #deeply because it would mean we going into the elements of
-                #the child group. We just want a reference to the group's
-                #name.
+                # Iterating the components usually returns the group itself so
+                # we check to make sure our component isn't the group itself.
+                # Also, for groups within groups, we don't want to traverse
+                # deeply because it would mean we going into the elements of
+                # the child group. We just want a reference to the group's
+                # name.
                 if component == group or (is_group and component.ref is None):
                     continue
                 if is_group:
-                    #self._build_group(component)
-                    #Since we don't want to dive deeper into child groups,
-                    #continue.
+                    # self._build_group(component)
+                    # Since we don't want to dive deeper into child groups,
+                    # continue.
                     continue
 
                 if isinstance(component, XsdElement):
                     self._build_element(component)
                 else:
-                    #Seems we've encountered a component we haven't prepared
-                    #for. Make it known.
-                    raise Exception(f'Unhandled component {component} during build process!')
+                    # Seems we've encountered a component we haven't prepared
+                    # for. Make it known.
+                    raise Exception(
+                        f'Unhandled component {component} during build process!')
 
     def _build_element(self, element):
         from .element import Element
@@ -106,5 +133,11 @@ class Buildable(Super):
         return self._elem
 
     def get_groups(self):
+        """Get the groups associated with this object.
+
+        Returns:
+            dict: Returns a dict with group names as keys and Group objects
+                as values.
+        """
         self._build()
         return self._group
