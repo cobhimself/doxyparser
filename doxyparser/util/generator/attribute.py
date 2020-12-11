0543a8b830a50e3347e1bfa5b67f43faf561ad83
Collin Brooks <cbrooks@sentryds.com>
2020-12-11 10:02:35 -0600

    Update attribute documentation

--- doxyparser/util/generator/attribute.py
+++ doxyparser/util/generator/attribute.py
@@ -1,6 +1,8 @@
+"""Responsible for holding information relating to xsd schema attributes
+"""
 from xmlschema.validators.attributes import XsdAnyAttribute
-
 from .typeable import Typeable
+
 
 class Attribute(Typeable):
     """Class responsible for holding information relating to xsd schema
@@ -13,15 +15,38 @@ class Attribute(Typeable):
         super().__init__(element)
 
     def get_name(self):
+        """Get the name of this attribute.
+
+        Returns:
+            string: The local name of this attribute
+        """
         return self._definition.local_name
 
     def is_any_attribute(self):
+        """Determine if this attribute is an XsdAnyAttribute.
+
+        Returns:
+            bool
+        """
         return isinstance(self.get_definition(), XsdAnyAttribute)
 
     def is_enum(self):
+        """Determine whether or not this attribute has enum values.
+
+        Returns:
+            bool
+        """
         return len(self.get_enum_values()) > 0
 
     def get_enum_values(self):
+        """Get the enum values associated with this attribute.
+
+        For types that are union types, we dig deep into them to find whether
+        or not any of their members have enum values.
+
+        Returns:
+            list: A list of enum values
+        """
 
         if self._enums is None:
             resolved = None
@@ -38,16 +63,23 @@ class Attribute(Typeable):
                 self._enums = []
             else:
                 if resolved.is_simple():
-                    self._enums = [] if resolved.enumeration is None or resolved.enumeration == [''] else resolved.enumeration
+                    self._enums = [] if resolved.enumeration is None or resolved.enumeration == [
+                        ''] else resolved.enumeration
 
         return self._enums
 
-
     def is_required(self):
+        """Determine whether or not this attribute is required.
+
+        Returns:
+            bool
+        """
         return self._definition.is_required
 
     def is_optional(self):
-        return self._definition.is_optional
+        """Determine whether or not this attribute is optional.
 
-    def get_decorator(self):
-        return "@attr('{}', {})".format(self.get_name(), self.get_type())
+        Returns:
+            bool
+        """
+        return self._definition.is_optional
